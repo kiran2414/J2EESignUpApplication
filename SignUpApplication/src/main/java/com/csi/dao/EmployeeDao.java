4775fecf8ec31c22fec53a0d8b28a3b5a5131053
kiranjagtap24 <kjagtap2414@gmail.com>
2022-08-02 18:40:52 +0530

    added code for signup functionality

---
+++ SignUpApplication/src/main/java/com/csi/dao/EmployeeDao.java
@@ -0,0 +1,7 @@
+package com.csi.dao;
+
+import com.csi.model.Employee;
+
+public interface EmployeeDao {
+	public void signup(Employee employee);
+}
---
+++ SignUpApplication/src/main/java/com/csi/service/EmployeeService.java
@@ -0,0 +1,7 @@
+package com.csi.service;
+
+import com.csi.model.Employee;
+
+public interface EmployeeService {
+	public void signup(Employee employee);
+}
